8a627f55460f7a03cf401f3f202ae63f4685b33d
Prashant Malla <prashant.malla977@gmail.com>
2021-04-22 14:46:54 +0545

    added: learn styling css

---
+++ src/components/Inline.js
@@ -0,0 +1,15 @@
+const heading = {
+    fontSize: '72px'
+}
+const primary = {
+    color: 'blue'
+}
+const Inline = () => {
+    return (
+        <div>
+            <h2 style={heading, primary}>Inline Example</h2>
+        </div >
+    );
+}
+
+export default Inline;
